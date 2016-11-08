b51a571705cf272c7ba0a499f577a100bbe7e77f
yboyer <bhyw180@gmail.com>
2016-11-08 01:16:28 +0100

    Add 404 page

--- website/app/routes.php
+++ website/app/routes.php
@@ -1,12 +1,19 @@
-
 <?php
 
-$app->get('/', function () use ($app) {
-    $messages = $app['dao.messages']->findAll();
+use Symfony\Component\HttpFoundation\Request;
 
+$app->get('/', function () use ($app) {
     return $app['twig']->render('home.html.twig');
 });
 
 $app->get('/search', function () use ($app) {
     return $app['twig']->render('search.html.twig');
+});
+
+// Error handler
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    switch($code) {
+        case 404:
+            return $app['twig']->render('404.html.twig');
+    }
 });
